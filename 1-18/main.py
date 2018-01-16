674f5752ce3b9303d86208d2633b2784b35ee898
Josh Higham <josh@higham.com>
2018-01-16 15:33:27 -0700

    Prep for 1-18.

---
+++ 1-18/main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+"""
+Demonstration for Sections 2.1 to 2.7
+
+2.1 Introduction 32
+
+2.2 Writing a Simple Program 32
+
+2.3 Reading Input from the Console 34
+
+2.4 Identifiers 36
+
+2.5 Variables, Assignment Statements, and Expressions 36
+
+2.6 Simultaneous Assignments 38
+
+2.7 Named Constants 39
+
+"""
+
+# 2.1 Intro
+
+
+
+# 2.2 Simple Program
+
+
+
+# 2.3 Reading input from the console
+
+
+
+# 2.4 Identifiers
+
+
+
+# 2.5 Variables, Assignment, and Expressions
+
+
+
+# 2.6 Simultaneous Assignment
+
+
+
+# 2.7 Constants
