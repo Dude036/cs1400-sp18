bd66bb47d11d05a713d397c352be426cf5247382
Josh Higham <josh@higham.com>
2018-01-16 15:54:08 -0700

    Final Prep for Thursday

--- 1-18/main.py
+++ 1-18/main.py
@@ -19,28 +19,47 @@ Demonstration for Sections 2.1 to 2.7
 
 """
 
-# 2.1 Intro
-
-
-
 # 2.2 Simple Program
+farenheit = 32
+celcius = (farenheit - 32) * 5/9
 
+print(farenheit, "degrees Farenheit is", celcius, "degrees Celcius")
 
 
 # 2.3 Reading input from the console
+variable = int(input("Enter a degree in Farenheit: "))
+celcius = (variable - 32) * 5/9
 
-
-
-# 2.4 Identifiers
-
+print(variable, "degrees Farenheit is", celcius, "degrees Celcius")
 
 
 # 2.5 Variables, Assignment, and Expressions
-
-
+x = 1
+do_a_thing = 5
 
 # 2.6 Simultaneous Assignment
+c = 15
+a = 10
+b = 5
 
+print("A = ", a)
+print("B = ", b)
+
+a, b = b, a
+
+print("A = ", a)
+print("B = ", b)
+
+a = b = c = 20
+
+print("A = ", a)
+print("B = ", b)
+print("C = ", c)
 
 
 # 2.7 Constants
+
+CONSTANT = 42
+constant = 0
+
+print(CONSTANT)
