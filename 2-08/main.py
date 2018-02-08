9e817c9627c08f0f122f07f368967c6bf51049c9
Josh Higham <josh@higham.com>
2018-02-08 11:05:24 -0700

    Lesson Planned.

---
+++ 2-08/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+
+'''
+Feb 16 - Exam 1
+
+Review Next week to cover everything. Bring questions
+'''
+
+# Boolean Values and how to get them
+
+a = True
+print(a)
+
+b = 6 >= 10
+print(b)
+'''
+# Control Block Points
+if a:
+	print("A is True")
+elif b:
+	print("B is True")
+else:
+	print("Both are False")
+
+# Randomness
+import random
+
+# randint(<Low Value>, <High Value>) Inclusive
+print(random.randint(0, 5))
+
+'''
+
+# # # # # # #  #  # # # # # # # 
+# Case Study: The Stupid Tax  #
+# # # # # # #  #  # # # # # # #
+# Pick two numbers from 0 to 9, and see if you can match them to the slot
+
