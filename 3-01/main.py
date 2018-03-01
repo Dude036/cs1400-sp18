862a2c874434b13379b7e7384180b0e5361ccdca
Josh Higham <josh@higham.com>
2018-03-01 11:28:28 -0700

    Classroom Prep.

---
+++ 3-01/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+'''
+Moar functions
+	- Return statements
+	- A better way to main()
+	- Many arguements
+	- Scope
+
+Homework Help
+	- Strings vs. Numbers
+	- Luhn
+'''
+def func():
+	print("I did a thing.")
+
+def ret_func():
+	return "I did a thing"
+
+
+
+'''
+Case Study: 
+'''
